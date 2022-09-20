d23423d5f69f397d59607b48e009fb3637099800
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-09-20 20:12:21 +0200

    add performances charts component

--- src/components/Performances/index.jsx
+++ src/components/Performances/index.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
 
 import './performances.css';
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  ResponsiveContainer,
+} from 'recharts';
+import PropTypes from 'prop-types';
 
+/**
+ * Display user's performances chart
+ * @component
+ * @param {Array} performance - array of performances datas
+ * @returns {JSX.Element} PerformanceGraph component
+ */
 const Performances = ({ performance }) => {
-  return <div></div>;
+  //console.log(performance)
+  return (
+    <div className='performancesChart'>
+      <ResponsiveContainer width='100%' aspect={1}>
+        <RadarChart
+          margin={{ top: 30, right: 30, bottom: 30, left: 30 }}
+          style={{ backgroundColor: '#282D30' }}
+          data={performance}
+        >
+          <PolarGrid radialLines={false} />
+          <PolarAngleAxis
+            dataKey='kind'
+            tickLine={false}
+            axisLine={false}
+            dy={5}
+            stroke='#FFF'
+            tick={{ fill: '#FFFFFF', fontSize: 12 }}
+          />
+          <PolarRadiusAxis tick={false} tickCount={6} axisLine={false} />
+          <Radar dataKey='value' fill='#FF0101' fillOpacity={0.6} />
+        </RadarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Performances;
+
+//Proptypes
+Performances.propTypes = {
+  performance: PropTypes.array.isRequired,
+};
